b82cfffe80f7bfbc2b992f467aebb08513e09c35
109s83 <soojeong@kku.ac.kr>
2026-06-19 16:40:12 +0900

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='text_analyzer',
+    version='1.0.0',
+    author='이수정',
+    description='텍스트 분석 도구 패키지',
+    packages=find_packages(),
+    python_requires='>=3.8',
+)
